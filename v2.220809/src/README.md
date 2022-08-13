1e38f8a9ebec5c5068042db38d6d4a81ad937f4b
Jean Guillaume <jgi@jgwill.com>
2022-08-13 16:04:56 -0400

    import:Pto of new Zeusis infrastructure on 16.04
    * Expect installing Python ML package to work (LZMA)

---
+++ v2.220809/src/README.md
@@ -0,0 +1,9 @@
+# Current Zeuzis platform
+
+* Uses Python 3.7.1 (even if SQLMLService has 3.7.2)
+* sqlmlutil (pip install sqlmlutils) has issues with LZMA
+* another Infrastructure is in building on both Ubuntu 16/18 using Python 3.7.2 which has LZMA
+
+
+
+https://github.com/pypa/setuptools.git
---
+++ v2.220809/src/_env.sh
@@ -0,0 +1,5 @@
+export dockertag="guillaumeai/server:zeuz-mssql-ml-ubuntu-16.04-220809-py3.7.2-lzma"
+export containername=zeuz-mssql-mls-220616
+export containername=zeuzis
+#zaml
+export stcgoal="SSIS, SQL and SQL Server Machine Learning Services (Python and R) on Docker and have a fix for the MSSQLML Python install SQLMLUtils (LZMA)"
