4bfd386c409cd7b615a088906ee56acb95f43de5
JGWill Gaia.WSL <jgi@jgwill.com>
2022-11-20 16:09:32 -0500

    v3

---
+++ v2.220809/src/ssis-mssql-zeuz-ubuntu18/src/_env.sh
@@ -0,0 +1,8 @@
+export tlidtag=221120
+export dockertag="guillaumeai/server:zeuz-mssql-ml-ubuntu-18.04-ssis-py3.7.2-lzma-tfx-$tlidtag"
+
+#guillaumeai/server:zeuz-mssql-ml-ubuntu-18.04-ssis-py3.7.2-lzma
+export containername=zeuz-mssql-mls-$tlidtag-ssis
+export containername=zeuzisV3
+#zaml
+export stcgoal="SSIS, SQL and SQL Server Machine Learning Services (Python and R) on Docker and have a fix for the MSSQLML Python install SQLMLUtils (LZMA)"
---
+++ v2.220809/src/ssis-mssql-zeuz-ubuntu18/src/run-zeuz-220615.sh
@@ -0,0 +1,34 @@
+
+EULA_STUFF="-e MSSQL_PID=Developer -e ACCEPT_EULA=Y -e ACCEPT_EULA_ML=Y "
+ddir=/mnt/c/var/lib/mssql/data
+mkdir -p $ddir
+. .env
+mlocation=/data
+
+RUNMODE="run -d"
+dockertag="docker.io/guillaumeai/server:mssql-mls-220312"
+
+dockertag="docker.io/guillaumeai/server:zeuz"
+
+dockertag="docker.io/guillaumeai/server:mssql-mls-220312-ssis"
+dockertag="docker.io/guillaumeai/server:zeuz-mssql-ml-ubuntu-16.04-ssis"
+dockertag="docker.io/guillaumeai/server:zeuz-mssql-ml-ubuntu-18.04-ssis-py3.7.2-lzma"
+
+# dockertag="docker.io/jgwill/zeus:zeuzis-2206150918"
+containername=zeuzis
+
+if [ "$1" == "--rm" ]; then docker rm -f $containername ;fi
+
+MSSQL_PORTMAP="-p 1433:1433"
+SSIS_PORTMAP="-p 3882:3882"
+
+cmd="docker $RUNMODE $EULA_STUFF --name $containername \
+     -e MSSQL_SA_PASSWORD="$SAPWD" \
+     -v $ddir:$mlocation -v $(pwd):/work \
+     $MSSQL_PORTMAP $SSIS_PORTMAP \
+     $dockertag"
+
+echo "$cmd"
+$cmd
+
+#jgwill/zeus:zeuz-2206150813 
