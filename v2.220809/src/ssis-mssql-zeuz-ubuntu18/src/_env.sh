b0b3b1543c08448fb7e2e25d6234b05c0e783c11
JGWill Gaia.WSL <jgi@jgwill.com>
2022-11-20 17:23:37 -0500

    done

--- v2.220809/src/ssis-mssql-zeuz-ubuntu18/src/_env.sh
+++ v2.220809/src/ssis-mssql-zeuz-ubuntu18/src/_env.sh
@@ -6,3 +6,8 @@ export containername=zeuz-mssql-mls-$tlidtag-ssis
 export containername=zeuzisV3
 #zaml
 export stcgoal="SSIS, SQL and SQL Server Machine Learning Services (Python and R) on Docker and have a fix for the MSSQLML Python install SQLMLUtils (LZMA)"
+
+echo "see in : /c/Caishen/Caishen/src/Caishen/SDS/OdinStrategyRunner/v2/zeusis"
+echo "          for zeuzis running"
+echo "          then we commit the running using . wf-commit... in : /mnt/c/Caishen/Caishen/src/Caishen/Databases/SpiderDbDatabase/gia-mssql/ZEUS"
+
