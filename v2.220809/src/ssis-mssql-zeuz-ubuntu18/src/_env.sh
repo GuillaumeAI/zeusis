7f49724b2da9bf50ec5676959a7e406b3160c9fa
JGWill Gaia.WSL <jgi@jgwill.com>
2022-12-03 16:19:42 -0500

    SSIS(removed, issue but also deprecating)

--- v2.220809/src/ssis-mssql-zeuz-ubuntu18/src/_env.sh
+++ v2.220809/src/ssis-mssql-zeuz-ubuntu18/src/_env.sh
@@ -1,11 +1,12 @@
-export tlidtag=221120
-export dockertag="guillaumeai/server:zeuz-mssql-ml-ubuntu-18.04-ssis-py3.7.2-lzma-tfx-$tlidtag"
+export tlidtag=221203
+#export dockertag="guillaumeai/server:zeuz-mssql-ml-ubuntu-18.04-ssis-py3.7.2-lzma-tfx-$tlidtag"
+export dockertag="guillaumeai/server:zeuz-mssql-ml-ubuntu-18.04-py3.7.2-lzma-tfx-$tlidtag"
 
 #guillaumeai/server:zeuz-mssql-ml-ubuntu-18.04-ssis-py3.7.2-lzma
-export containername=zeuz-mssql-mls-$tlidtag-ssis
+export containername=zeuz-mssql-mls-$tlidtag
 export containername=zeuzisV3
 #zaml
-export stcgoal="SSIS, SQL and SQL Server Machine Learning Services (Python and R) on Docker and have a fix for the MSSQLML Python install SQLMLUtils (LZMA)"
+export stcgoal="SSIS(removed, issue but also deprecating), SQL and SQL Server Machine Learning Services (Python and R) on Docker and have a fix for the MSSQLML Python install SQLMLUtils (LZMA)"
 
 echo "see in : /c/Caishen/Caishen/src/Caishen/SDS/OdinStrategyRunner/v2/zeusis"
 echo "          for zeuzis running"
